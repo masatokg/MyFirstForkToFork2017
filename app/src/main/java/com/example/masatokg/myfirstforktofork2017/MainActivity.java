4ab4f48291333b022a08df7a4a1018e44020d057
masatokg <masatokg@asojuku.ac.jp>
2019-11-14 10:53:22 +0900

    フォークしたプロジェクトのPush

--- app/src/main/java/com/example/masatokg/myfirstforktofork2017/MainActivity.java
+++ app/src/main/java/com/example/masatokg/myfirstforktofork2017/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // フォークしたプロジェクトの編集
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
